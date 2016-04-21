4e2b42f754d12e20a25d6b4e92ac623370d0a527
Daiern <mathew.conyers@gmail.com>
2016-04-20 22:46:09 -0700

    add gamestate and gameinstance

---
+++ Source/UnitSelection/UnitSelectionGameInstance.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "UnitSelectionGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNITSELECTION_API UUnitSelectionGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
